9990c12d572c6bc9d4eba0e07e5be360a3ed74e1
Be4t5 <thomas.traverso54@gmail.com>
2019-08-03 19:27:31 +0200

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -279,7 +279,9 @@ public class InAppBrowser extends CordovaPlugin {
             this.cordova.getActivity().runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
+	          try{
                     dialog.show();
+                  }catch (Exception e ){}
                 }
             });
             PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
